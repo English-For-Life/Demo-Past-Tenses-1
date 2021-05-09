2d3b1d36e04e0a0aa0ee0c83eebcc56d552e5a67
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:11:57 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I told John not to drive, because he _____ all night.",
+        options:["had been drinking", "had drunk", "was drinking", "drank"],
+        answer: 0
+    },
+    {
+     q:"Why did you change the channel? I ______ that movie.",
+        options:["watched", "was watching", "had watched", "had been watching"],
+        answer: 1   
+    },
+    {
+        q:"When I _____ out of the shower the lights _____ .",
+        options:["got / were getting off", "had got / were getting off", "was getting / went off", "had been getting / had got off"],
+        answer: 2
+    },
+    {
+        q:"When I saw Clara I realised that she ______ much in all those years.",
+        options:["didn't change", "wasn't changing", "hadn't been changing", "hadn't changed"],
+        answer: 3
+    },
+    {
+        q:"When he entered the room somebody _____ him in the head.",
+        options:["hit", "was hitting", "had hit", "had been hitting"],
+        answer: 0
+    }
+]
